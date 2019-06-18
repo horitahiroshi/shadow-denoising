b548d46a07fdf627c0144be7c34c8522fae9e158
horitahiroshi <horitahiroshi@gmail.com>
2019-06-18 16:28:01 -0300

    python script with complete algorithm of shadow removal

--- src/shadow-removing.py
+++ src/shadow-removing.py
@@ -8,42 +8,209 @@ Final Project 2019: Shadow Removing
 # -*- coding: utf-8 -*-
 # importing packages
 import numpy as np
-from scipy.fftpack import fftn, ifftn, fftshift
 import imageio
 import utils
 import kmeans
 import matplotlib.pyplot as plt
 import ipdb
 
+debug = True
+
 """
-Enhancement routine
+Shadow regions detection.
 """
-def find_shadow():
-    result = 0
-    return result
+def find_shadows(img):
+    # compute grayscale luminance image, which is the database for further clustering step.
+    luminance_dataset = utils.compute_luminance(img)
+
+    # set the centroids to find the 3 regions: lit, penumbra and umbra.
+    luminance_centroids = np.array([[np.min(luminance_dataset)], 
+                                    [np.max(luminance_dataset)/2], 
+                                    [np.max(luminance_dataset)]])
+
+    # clustering image into lit, penumbra and umbra regions
+    lpu = kmeans.kmeans_routine(luminance_dataset, luminance_centroids, luminance_centroids.shape[0], 5)
+
+    return lpu
 
 """
 Restoration routine
 """
-def restore_shadowed_region():
-    unshadowed_region = 0
+def restore_shadowed_region(img, lpu):
+    if(debug):
+        print("Creating lit, penumbra and umbra masks...")
+    # binary masks for each regions (lit, penumbra and umbra)
+    lit_mask = np.zeros((img.shape[0], img.shape[1]))
+    lit_mask[np.where(lpu==3)] = 1
+
+    penumbra_mask = np.zeros((img.shape[0], img.shape[1]))
+    penumbra_mask[np.where(lpu==2)] = 1
+
+    umbra_mask = np.zeros((img.shape[0], img.shape[1]))
+    umbra_mask[np.where(lpu==1)] = 1
+
+    # the original image cut into the 3 regions
+    lit_img = np.zeros(img.shape, dtype=np.uint8)
+    penumbra_img = np.zeros(img.shape, dtype=np.uint8)
+    umbra_img = np.zeros(img.shape, dtype=np.uint8)
+    for i in range(3):
+        lit_img[:,:,i] = np.multiply(lit_mask,img[:,:,i])
+        penumbra_img[:,:,i] = np.multiply(penumbra_mask,img[:,:,i])
+        umbra_img[:,:,i] = np.multiply(umbra_mask,img[:,:,i])
+
+    if(debug):
+        print("LPU (Lit-Penumbra-Umbra) masks created!\n")
+        print("Clustering lit regions...")
+    # clutering lit region into 3 clusters based on color and position coordinates
+    lit_clusters_mask = np.zeros((img.shape[0],img.shape[1]),dtype=np.uint8)
+
+    indx = np.arange(0, lit_img.shape[1])
+    indy = np.arange(0, lit_img.shape[0])
+    x,y = np.meshgrid(indx, indy)
+    lit_dataset = np.dstack((lit_img, x, y))
+    
+    lit_centroids=np.array([[255,0,0,img.shape[1]//2,img.shape[0]-1],
+                            [0,255,0,img.shape[1]//2,img.shape[0]//2],
+                            [0,0,255,img.shape[1]//2,0]])
+
+    lit_clusters_mask = kmeans.kmeans_routine(lit_dataset, lit_centroids, 3, 1)
+
+    if(debug):
+        print("Lit regions clustered!\n")
+        print("Clustering penumbra regions...")
+    # clutering penumbra region into 3 clusters based on color
+    penumbra_clusters_mask = np.zeros((img.shape[0],img.shape[1]),dtype=np.uint8)
+
+    penumbra_dataset = penumbra_img
+
+    penumbra_centroids=np.array([[255,0,0],
+                            [0,255,0],
+                            [0,0,255]])
+
+    penumbra_clusters_mask = kmeans.kmeans_routine(penumbra_dataset, penumbra_centroids, 3, 1)
+
+    if(debug):
+        print("Penumbra regions clustered!\n")
+        print("Clustering umbra regions...")
+    # clutering umbra region into 3 clusters based on color
+    umbra_clusters_mask = np.zeros((img.shape[0],img.shape[1]),dtype=np.uint8)
+
+    umbra_dataset = umbra_img
+
+    umbra_centroids=np.array([[255,0,0],
+                            [0,255,0],
+                            [0,0,255]])
+
+    umbra_clusters_mask = kmeans.kmeans_routine(umbra_dataset, umbra_centroids, 3, 1)
+
+    # Plot the masks, image regions and the resulting clusters of each respective regions
+    plt.figure(figsize=(16,10))
+    plt.subplot(331);plt.imshow(lit_mask);plt.title("Lit mask")
+    plt.subplot(332);plt.imshow(penumbra_mask);plt.title("Penumbra mask")
+    plt.subplot(333);plt.imshow(umbra_mask);plt.title("Umbra mask")
+    plt.subplot(334);plt.imshow(lit_img);plt.title("Lit regions")
+    plt.subplot(335);plt.imshow(penumbra_img);plt.title("Penumbra regions")
+    plt.subplot(336);plt.imshow(umbra_img);plt.title("Umbra regions")
+    plt.subplot(337);plt.imshow(lit_clusters_mask);plt.title("Lit clusters");plt.colorbar()
+    plt.subplot(338);plt.imshow(penumbra_clusters_mask);plt.title("Penumbra clusters");plt.colorbar()
+    plt.subplot(339);plt.imshow(umbra_clusters_mask);plt.title("Umbra clusters");plt.colorbar()
+
+    if(debug):
+        print("Umbra regions clustered!\n")
+        print("Removing shadows from image...")
+    # The shadow removal part
+    # Create a copy of original image to receive results
+    unshadowed_region = np.copy(img)
+
+    # Create masks of each clusters of each regions (umbra, penumbra and lit)
+    penumbra_blue_mask = np.zeros(penumbra_clusters_mask.shape, dtype=np.uint8)
+    penumbra_blue_mask[np.where(penumbra_clusters_mask == 3)] = 1
+
+    penumbra_red_mask = np.zeros(penumbra_clusters_mask.shape, dtype=np.uint8)
+    penumbra_red_mask[np.where(penumbra_clusters_mask == 1)] = 1
+
+    penumbra_green_mask = np.zeros(penumbra_clusters_mask.shape, dtype=np.uint8)
+    penumbra_green_mask[np.where(penumbra_clusters_mask == 2)] = 1
+
+    umbra_blue_mask = np.zeros(umbra_clusters_mask.shape, dtype=np.uint8)
+    umbra_blue_mask[np.where(umbra_clusters_mask == 3)] = 1
+
+    umbra_red_mask = np.zeros(umbra_clusters_mask.shape, dtype=np.uint8)
+    umbra_red_mask[np.where(umbra_clusters_mask == 1)] = 1
+
+    umbra_green_mask = np.zeros(umbra_clusters_mask.shape, dtype=np.uint8)
+    umbra_green_mask[np.where(umbra_clusters_mask == 2)] = 1
+
+    lit_red_mask = np.zeros(lit_clusters_mask.shape, dtype=np.uint8)
+    lit_red_mask[np.where(lit_clusters_mask==1)] = 1
+
+    lit_green_mask = np.zeros(lit_clusters_mask.shape, dtype=np.uint8)
+    lit_green_mask[np.where(lit_clusters_mask == 2)] = 1
+
+    # equalize blue penumbra region with red penumbra region
+    unshadowed_region = utils.remove_shadow(unshadowed_region, penumbra_blue_mask, penumbra_red_mask)
+
+    # equalize blue umbra region with red umbra region
+    unshadowed_region = utils.remove_shadow(unshadowed_region, umbra_blue_mask, penumbra_red_mask)
+
+    # remove umbra on road region
+    umbra_road_mask = umbra_blue_mask
+    unshadowed_region = utils.remove_shadow(unshadowed_region, umbra_road_mask, lit_red_mask)
+
+    # remove umbra on woods
+    umbra_woods_mask = umbra_red_mask
+    unshadowed_region = utils.remove_shadow(unshadowed_region, umbra_woods_mask, penumbra_blue_mask)
+
+    # remove penumbra on road region
+    penumbra_road_mask = penumbra_blue_mask + penumbra_red_mask
+    unshadowed_region = utils.remove_shadow(unshadowed_region, penumbra_road_mask, lit_red_mask)
+
+    # equalize green penumbra region
+    unshadowed_region = utils.remove_shadow(unshadowed_region, penumbra_green_mask, lit_green_mask)
+    unshadowed_region = utils.remove_shadow(unshadowed_region, umbra_green_mask, penumbra_green_mask)
+
+    if(debug):
+        print("Shadows removed!\n")
+        
     return unshadowed_region
 
 def main():
+    if(debug):
+        print("Reading image file...")
     # read shadowed image to be processed
-    g = imageio.imread("../images/shadow5.jpg")
-    # ipdb.set_trace()
-    # rbin = utils.limiarization(g[:,:,0],0.5)
-    rbin = kmeans.main(g, 1, 3, 5)
+    filename = "../images/shadow5.jpg"
+    g = imageio.imread(filename)
 
-    plt.figure(figsize=(16,16))
-    plt.subplot(121)
-    plt.imshow(g)
-    plt.title("original")
-    plt.subplot(122)
-    plt.imshow(rbin)
-    plt.title("clusterized")
+    if(debug):
+        print("Image read!\n")
+        print("Filtering image...")
+    # filter the color image to remove noise
+    for i in range(3):
+        g[:,:,i] = utils.adaptive_denoising(g[:,:,i],3,0.005,"average")
+                
+    if(debug):
+        print("Image filtered!\n")
+        print("Finding shadow (lit, penumbra, umbra) regions...")
+    # call method to find the shadowed regions
+    lpu = find_shadows(g)
+    # remove noise by applying adaptive denoising
+    lpu = utils.adaptive_denoising(lpu, 3, 0.005, "robust",1)
+    
+    if(debug):
+        print("Shadow detected!\n")
+        print("Initiating shadow removal routine.\n")
+    
+    f = restore_shadowed_region(g, lpu)
+
+    plt.figure()
+    plt.subplot(221);plt.imshow(g);plt.title("original image")
+    plt.subplot(222);plt.imshow(f); plt.title("result of shadow removal")
     plt.show()
+
+    return 0
+
+    
+    
 
 # Call main function
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,9 @@ Library with general image processing methodes.
 # -*- coding: utf-8 -*-
 import numpy as np
 
+def compute_luminance(img):
+    return(0.299*img[:,:,0] + 0.587*img[:,:,1] + 0.114*img[:,:,2])
+
 # return image's histogram in a single array of values
 def histogram(img, range_values):
     # creates an empty histogram array with size proportional to the number of pixel values 
@@ -25,15 +28,43 @@ def histogram(img, range_values):
             
     return histogram
 
+# function for erosion operation
+def erode(img, k):
+    result = np.copy(img)
+    
+    N,M = img.shape
+    for x in range(k//2,N-k//2):
+        for y in range(k//2,M-k//2):
+            if(img[x,y] == 0 and (img[x-1,y-1]==1 or img[x-1,y]==1 or img[x-1,y+1]==1 or 
+                                  img[x,y-1]==1 or img[x,y]==1 or img[x,y+1]==1 or
+                                  img[x+1,y-1]==1 or img[x+1,y]==1 or img[x+1,y+1]==1)):
+                for kx in range(-k//2, (k//2)+1):
+                    for ky in range(-k//2, (k//2)+1):
+                        result[x+kx, y+ky] = 0
+    
+    return result
+
 # readjustment of range of values
-def range_adjustment(img, minvalue=0, maxvalue=255):
-    currmin = np.min(img)
-    currmax = np.max(img)
+def range_adjustment(img,minvalue=0,maxvalue=255):
+    try:
+        layers = img.shape[2]
+    except:
+        layers = 1
     
     img_u8 = np.zeros(img.shape, dtype=np.uint8)
-    for x in range(img.shape[0]):
-        for y in range(img.shape[1]):
-            img_u8[x,y] = ((img[x,y]-currmin)*(maxvalue - minvalue)//(currmax - currmin)) + minvalue
+    if(layers > 1):
+        for z in range(layers):
+            currmin = np.min(img[:,:,z])
+            currmax = np.max(img[:,:,z])
+            for x in range(img.shape[0]):
+                for y in range(img.shape[1]):
+                    img_u8[x,y,z] = ((img[x,y,z]-currmin)*(maxvalue - minvalue)//(currmax - currmin)) + minvalue
+    else:
+        currmin = np.min(img)
+        currmax = np.max(img)
+        for x in range(img.shape[0]):
+            for y in range(img.shape[1]):
+                img_u8[x,y] = ((img[x,y]-currmin)*(maxvalue - minvalue)//(currmax - currmin)) + minvalue
     
     return img_u8 
 
@@ -43,3 +74,87 @@ def limiarization(img, thr = 0.5):
     mask = np.zeros(img.shape, dtype = np.uint8)
     mask[np.where(img < threshold)] = 1
     return mask
+
+# Adaptive Denoising
+def adaptive_denoising(g, k, gamma, denoise_mode, iterations=1):
+    # calculate the center of kernel
+    a = k//2
+
+    # create a zero matrix to fill with denoising result
+    r = np.zeros(g.shape)
+    
+    for i in range(iterations):
+        # declaration of image dispersion measure, local dispersion measure and centrality measure
+        gdisp = 0.0
+        ldisp = 0.0
+        centr = 0.0
+
+        # compute the estimation of the image global dispersion in a determined subregion of the image accordig to the assignment description
+        subregion = g[0:(g.shape[0]//6), 0:(g.shape[1]//6)]
+        if(denoise_mode == "average"):
+            gdisp = np.std(subregion)
+        if(denoise_mode == "robust"):
+            q1, c, q2 = np.percentile(subregion, [25, 50, 75])
+            gdisp = q2 - q1
+
+        # "If the dispersion measure computed for some image is 0, then manually set it to 1"
+        if(gdisp == 0):
+            gdisp = 1
+
+        # loop to run all over the image and denoise it
+        for x in range(g.shape[0]):
+            for y in range(g.shape[1]):
+                # if running at the border of the image, when the filter kernel exceeds the limits of the image
+                # then the resulting value is the copy of original image's pixel.
+                if(x<a or x>(g.shape[0]-a) or y<a or y>(g.shape[1]-a)):
+                    r[x,y] = g[x,y]
+
+                # else compute the filter
+                else:
+                    # Sx is the image region, kernel size, to be filtered
+                    Sx = g[x-a:(x+a+1), y-a:(y+a+1)]
+
+                    # compute the local dispersion and centrality measure, according to denoise mode (average/robust)
+                    if(denoise_mode == "average"):
+                        # if denoise mode is average, dispersion is standard deviation and the centrality is the mean of region Sx
+                        ldisp = np.std(Sx)
+                        centr = np.mean(Sx)
+                    elif(denoise_mode == "robust"):
+                        # if denoise mode is robust, dispersion is interquartile range and the centrality is the median of region Sx
+                        q1, centr, q2 = np.percentile(Sx, [25, 50, 75])
+                        ldisp = q2 - q1
+
+                    # "If during the denoising step, any local dispersion measure is 0, then set it so that local dispersion = global dispersion"
+                    if(ldisp == 0):
+                        ldisp = gdisp
+
+                    # formula of adaptive denoising
+                    r[x,y] = int(g[x,y] - gamma*(gdisp/ldisp)*(g[x,y]-centr))
+    
+    return r
+
+
+# Method to remove shadow
+def remove_shadow(img, mask, ref_mask):
+    mask_coordinates = np.where(mask==1)
+    ref_coordinates = np.where(ref_mask==1)
+
+    # loop for each color layer
+    for c in range(3):
+        refmean = np.average(img[np.where(ref_mask==1)][:,c])
+        # refstd = np.std(img[np.where(ref_mask==1)][:,c])
+        refmin = np.min(img[ref_coordinates][:,c])
+        refmax = np.max(img[ref_coordinates][:,c])
+        
+        # maskmean = np.average(img[np.where(mask==1)][:,c])
+        maskmin = np.min(img[mask_coordinates][:,c])
+        maskmax = np.max(img[mask_coordinates][:,c])
+        
+        # diff = refmean - maskmean
+
+        for i in range(mask_coordinates[0].shape[0]):
+            x,y,z=mask_coordinates[0][i],mask_coordinates[1][i],c
+            img[x,y,z]=((float(img[x,y,z])-maskmin)*(refmax - refmin)/(maskmax - maskmin)) + refmin
+            # img[x,y,z]+= (diff + refstd)
+        
+    return img
